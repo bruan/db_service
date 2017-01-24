35c054d0d174791bda8f64cfd9f97592a21ef3ae
bruan <379171482.qq.com>
2017-01-24 18:00:03 +0800

    添加测试sql

---
+++ test/sql/test.sql
@@ -0,0 +1,40 @@
+/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: test
+Target Host: localhost
+Target Database: test
+Date: 2017/1/24 17:59:12
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for player_base
+-- ----------------------------
+CREATE TABLE `player_base` (
+  `id` int(10) unsigned NOT NULL,
+  `name` blob NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for player_extend
+-- ----------------------------
+CREATE TABLE `player_extend` (
+  `id` int(10) unsigned NOT NULL,
+  `data_set` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for player_extend1
+-- ----------------------------
+CREATE TABLE `player_extend1` (
+  `id` int(10) unsigned NOT NULL,
+  `data_set` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records 
+-- ----------------------------
